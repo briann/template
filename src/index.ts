dab93df9afc4fbf963134222f5ac2b5375d6ab98
Brian Ngo <brian@brianngo.net>
2016-11-30 12:39:25 -0800

    partial

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.warn("Hello world");
+import * as fs from "fs";
+import * as path from "path";
+
+import { printMatrix } from "./matrix";
+
+const args = process.argv.slice(2);
+if (args.length !== 1) {
+    throw new Error("Did not specify an input file.");
+}
+const inputFile = path.resolve(process.cwd(), args[0]);
+const contents = fs.readFileSync(inputFile).toString().split("\n");
+
+let matrix: string[][] = [];
+for (let i = 1; i < contents.length; i++) {
+    const row = contents[i].split(",");
+    matrix.push(row);
+}
+printMatrix(matrix);
